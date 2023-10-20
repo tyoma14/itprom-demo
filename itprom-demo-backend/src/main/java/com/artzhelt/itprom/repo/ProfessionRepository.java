28ac9542863ff627b9237a7e786667a3f6570c04
Artyom Zheltyshev <az@itprom.org>
2023-10-20 12:21:24 +0500

    Подключен Hibernate Envers и опробован на репозитории Профессий

--- itprom-demo-backend/src/main/java/com/artzhelt/itprom/repo/ProfessionRepository.java
+++ itprom-demo-backend/src/main/java/com/artzhelt/itprom/repo/ProfessionRepository.java
@@ -2,6 +2,8 @@ package com.artzhelt.itprom.repo;
 
 import com.artzhelt.itprom.domain.Profession;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface ProfessionRepository extends CrudRepository<Profession, Long> {
+public interface ProfessionRepository extends CrudRepository<Profession, Long>, RevisionRepository<Profession, Long, Long> {
+
 }
