9173ddaf8e67ec2299beb8dc3b573448065a1979
Artyom Zheltyshev <az@itprom.org>
2023-10-25 11:20:11 +0500

    - Сделаны модель и сервис - Состояние таблицы переведено в store

---
+++ itprom-demo-new-frontend/src/service/ProfessionService.ts
@@ -0,0 +1,17 @@
+import Profession from "@/model/Profession";
+import axios from "axios";
+
+export const professionService = {
+    async create(profession: Profession): Promise<Profession> {
+        return (await axios.post('/api/profession', profession)).data
+    },
+    async getAll(): Promise<Profession[]> {
+        return (await axios.get('/api/profession')).data
+    },
+    async update(id: number, profession: Profession): Promise<Profession> {
+        return (await axios.put(`/api/profession/${id}`, profession)).data
+    },
+    async delete(id: number): Promise<void> {
+        await axios.delete(`/api/profession/${id}`)
+    }
+}
--- itprom-demo-new-frontend/src/store/index.ts
+++ itprom-demo-new-frontend/src/store/index.ts
@@ -1,13 +1,29 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import Profession from "@/model/Profession";
 
-export default createStore({
+interface State {
+  professions: Profession[]
+}
+
+export default createStore<State>({
   state: {
+    professions: []
   },
   getters: {
+    getProfessions(state): Profession[] {
+      return state.professions
+    }
   },
   mutations: {
+    setProfessionsMutation(state, professions) {
+      state.professions.splice(0)
+      Array.prototype.push.apply(state.professions, professions)
+    }
   },
   actions: {
+    setProfessions(context, professions) {
+      context.commit('setProfessionsMutation', professions)
+    }
   },
   modules: {
   }
